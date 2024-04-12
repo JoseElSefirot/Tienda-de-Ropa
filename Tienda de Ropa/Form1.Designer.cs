5aa45ee721d0b649784da67673a9bdaf90053b98
DiegoMag <Diegomag2996@gmail.com>
2024-04-11 18:26:56 -0600

    ambio de nombre

--- Tienda de Ropa/Form1.Designer.cs
+++ Tienda de Ropa/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Tienda_de_Ropa
 {
-    partial class Form1
+    partial class Frm
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -29,10 +29,17 @@ namespace Tienda_de_Ropa
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Frm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Frm";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
